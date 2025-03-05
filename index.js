8bc4f6876a7ce1bf3b24c1f0fb907fea4342a1d7
betpractice116 <bestpractice8116@gmail.com>
2025-03-05 07:34:52 -0800

    feat: implemenr manager route

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
 const swaggerDocument = require('./swagger-output.json');
 
-require("./models");
+require('./models');
 
 // const authenticateToken = require('./src/middleware/authMiddleware'); // Import authentication middleware
-const auth = require("./src/router/authRouter");
-const vaRouter = require("./src/router/vaRouter");
-const clientRouter = require("./src/router/clientRouter");
-const adminRouter = require("./src/router/adminRouter");
-const managerRouter = require("./src/router/managerRouter");
-const uploadRouter = require("./src/router/uploadRouter");
-const { handleVerify } = require("./src/middleware/verifyToken");
-const roleVerify = require("./src/middleware/roleVerify");
+const auth = require('./src/router/authRouter');
+const vaRouter = require('./src/router/vaRouter');
+const clientRouter = require('./src/router/clientRouter');
+const adminRouter = require('./src/router/adminRouter');
+const managerRouter = require('./src/router/managerRouter');
+const uploadRouter = require('./src/router/uploadRouter');
+const { handleVerify } = require('./src/middleware/verifyToken');
+const roleVerify = require('./src/middleware/roleVerify');
 
-require('./utils/storageSetup.js')
-require("dotenv").config();
+require('./utils/storageSetup.js');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT;
@@ -54,7 +54,7 @@ app.get('/api/example', (req, res) => {
 });
 
 app.use(auth);
-app.use("/", uploadRouter);
+app.use('/', uploadRouter);
 //Authenticated User Access
 app.use(handleVerify);
 
--- src/router/vaRouter.js
+++ src/router/vaRouter.js
@@ -4,6 +4,11 @@ const app = express();
 
 const TaskController = require('../controller/TaskController');
 const ProjectController = require('../controller/ProjectController');
+const UserController = require('../controller/UserController');
+const ClientController = require('../controller/ClientController');
+const TimeTrackController = require('../controller/TimeTrackController');
+const KanbanController = require('../controller/KanbanController');
+const DownloadController = require('../controller/DownloadController');
 
 //Router for project CRUD action.
 router.post('/', async (req, res) => {
