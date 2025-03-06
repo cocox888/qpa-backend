a90b378aebaa53cab2686d82657e0960ae27e74c
betpractice116 <bestpractice8116@gmail.com>
2025-03-06 15:46:31 -0800

    fix: add activitylog

--- src/services/ActivityService.js
+++ src/services/ActivityService.js
@@ -1,7 +1,7 @@
-const ActivityLogs = require("../../models/ActivityLogs");
+const ActivityLogs = require('../../models/ActivityLogs');
 
-const { Op, fn, col, literal } = require("sequelize");
-const { getDateRanges } = require("../../utils/Time.cjs");
+const { Op, fn, col, literal } = require('sequelize');
+const { getDateRanges } = require('../../utils/Time.cjs');
 class ActivityService {
   async getAllActivityLogs(data) {
     try {
@@ -11,43 +11,96 @@ class ActivityService {
         startOfWeek,
         endOfWeek,
         startOfMonth,
-        endOfMonth,
+        endOfMonth
       } = getDateRanges();
 
       let dateFilter = {};
       const { filter } = data;
 
       switch (filter) {
-        case "today":
+        case 'today':
           dateFilter = { [Op.between]: [startOfToday, endOfToday] };
           break;
-        case "week":
+        case 'week':
           dateFilter = { [Op.between]: [startOfWeek, endOfWeek] };
           break;
-        case "month":
+        case 'month':
           dateFilter = { [Op.between]: [startOfMonth, endOfMonth] };
           break;
         default:
-          throw new Error("Invalid filter type");
+          throw new Error('Invalid filter type');
       }
       console.log(dateFilter);
 
       const logs = await ActivityLogs.findAll({
         attributes: [
-          [fn("DATE", col("created_at")), "log_date"], // Extract date from timestamp
-          [fn("COUNT", "*"), "log_count"], // Count logs per day
+          [fn('DATE', col('created_at')), 'log_date'], // Extract date from timestamp
+          [fn('COUNT', '*'), 'log_count'] // Count logs per day
         ],
         where: {
-          created_at: dateFilter,
+          created_at: dateFilter
         },
-        group: [fn("DATE", col("created_at"))], // Group by date
-        order: [[literal("log_date"), "ASC"]], // Order by date ascending
+        group: [fn('DATE', col('created_at'))], // Group by date
+        order: [[literal('log_date'), 'ASC']] // Order by date ascending
       });
 
       console.log(logs);
       const new_logs = logs.map((log) => ({
-        date: log.getDataValue("log_date"),
-        count: log.getDataValue("log_count"),
+        date: log.getDataValue('log_date'),
+        count: log.getDataValue('log_count')
+      }));
+
+      console.log(new_logs);
+      return new_logs;
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+  async getAllActivityLogsForUser(data) {
+    try {
+      const {
+        startOfToday,
+        endOfToday,
+        startOfWeek,
+        endOfWeek,
+        startOfMonth,
+        endOfMonth
+      } = getDateRanges();
+
+      let dateFilter = {};
+      const { filter } = data;
+
+      switch (filter) {
+        case 'today':
+          dateFilter = { [Op.between]: [startOfToday, endOfToday] };
+          break;
+        case 'week':
+          dateFilter = { [Op.between]: [startOfWeek, endOfWeek] };
+          break;
+        case 'month':
+          dateFilter = { [Op.between]: [startOfMonth, endOfMonth] };
+          break;
+        default:
+          throw new Error('Invalid filter type');
+      }
+      console.log(dateFilter);
+
+      const logs = await ActivityLogs.findAll({
+        attributes: [
+          [fn('DATE', col('created_at')), 'log_date'], // Extract date from timestamp
+          [fn('COUNT', '*'), 'log_count'] // Count logs per day
+        ],
+        where: {
+          created_at: dateFilter
+        },
+        group: [fn('DATE', col('created_at'))], // Group by date
+        order: [[literal('log_date'), 'ASC']] // Order by date ascending
+      });
+
+      console.log(logs);
+      const new_logs = logs.map((log) => ({
+        date: log.getDataValue('log_date'),
+        count: log.getDataValue('log_count')
       }));
 
       console.log(new_logs);
